77accf08e5e1d40d0dd6b4f7c7b142976167f9f1
PulatovSO <pulatovsirotillo@gmail.com>
2023-06-30 13:12:10 +0500

    feat: add sphere geometry

--- js/sphere.js
+++ js/sphere.js
@@ -2,10 +2,17 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+camera.position.z = 15;
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+// sphere geometry
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const sphere = new THREE.Mesh( geometry, material );
+
+scene.add( sphere );
 
 renderer.render( scene, camera );
